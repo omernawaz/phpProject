d6dfd0fc1019fcd395c0cd28309c235ab851151f
omernawaz <omer.nawaz@arbisoft.com>
2024-06-25 18:24:09 +0500

    Learning variables and printing

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
-echo "Hello World";
+
+//printing
+
+// echo "Hello World";
+
+// echo 'hello', 1.5;
+
+//print_r([1,2,3]);
+
+
+//variables
+
+$name = "Omer";
+$age = 22;
+$balance = 20.5;
+$grad = true;
+
+echo "$name is $age years old. They've got $balance balance, and graduated: ";
+
+var_dump($grad);
+
 
 ?>
+
