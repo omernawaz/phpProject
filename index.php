eebb123e7990ea4bcec24395c3f573914596fee8
omernawaz <omer.nawaz@arbisoft.com>
2024-06-26 07:54:42 +0500

    Conditionals

--- index.php
+++ index.php
@@ -12,14 +12,58 @@
 
 //variables
 
-$name = "Omer";
-$age = 22;
-$balance = 20.5;
-$grad = true;
+// $name = "Omer";
+// $age = 22;
+// $balance = 20.5;
+// $grad = true;
 
-echo "$name is $age years old. They've got $balance balance, and graduated: ";
+// echo "$name is $age years old. They've got $balance balance, and graduated: ";
 
-var_dump($grad);
+// var_dump($grad);
+
+
+//conditionals
+
+// $age = 20;
+
+// if($age > 17)
+// {
+//     echo "Old Enough <br>";
+// }
+// else
+// {
+//     echo "Too young <br>";
+// }
+
+// $hour = date("H");
+
+// if($hour < 12)
+//     echo "Good Morning";
+// else if($hour > 12 && $hour < 17)
+//     echo "Good Afternoon";
+// else 
+//     echo "Good Evening";
+
+// echo "<br>";
+
+// $posts = ["First"];
+
+// if(empty($posts))
+//     echo "No Posts";
+// else 
+//     echo $posts[0];
+
+// echo "<br>";
+
+// $post = !empty($posts) ? $posts[0] : "No Posts";
+
+// echo $post;
+
+// $post = $posts[0] ?? "No Posts";
+
+// echo $post;
+
+
 
 
 ?>
