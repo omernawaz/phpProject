d92712529f54b594b81669736f17cb6757f8fa76
omernawaz <omer.nawaz@arbisoft.com>
2024-06-26 08:59:27 +0500

    array functions

--- index.php
+++ index.php
@@ -142,15 +142,87 @@
 
 // echo $sum;
 
-$subtract = function($v1, $v2) {
-    return $v1 - $v2;
-};
+// $subtract = function($v1, $v2) {
+//     return $v1 - $v2;
+// };
 
-echo $subtract(10,5);
+// echo $subtract(10,5);
 
-$multiply = fn($v1, $v2) => $v1 * $v2;
+// $multiply = fn($v1, $v2) => $v1 * $v2;
 
-echo $multiply(10,5);
+// echo $multiply(10,5);
+
+
+// ARRAY FUNCTIONS;
+
+// $students = ['first' => "ali", 'second' => "omer", 'third' => "mujtaba"];
+
+// echo count($students);
+
+// var_dump(in_array('ali', $students));
+
+// $students['fourth'] = 'hussain';
+// array_push($students,'haseeb');
+// array_unshift($students, 'akbar');
+
+// array_pop($students); //based on numerical index
+// array_shift($students);  //based on numerical index too
+
+// $chunked = array_chunk($students,2);
+
+// print_r($chunked);
+
+// $new_array = array();
+// $new_array[] = 55;
+// array_push($new_array, 1,512,322,11,3);
+
+// print_r($new_array);
+
+// $arr1 = [1,2,3];
+// $arr2 = [4,5,6];
+
+// $arr3 = array_merge($arr1,$arr2);
+// $arr4 = [...$arr1, ...$arr2];
+
+// $positions = ['first', 'second', 'third'];
+// $students = ['omer', 'ali', 'farrukh'];
+
+// $arr4 = array_combine($positions,$students);
+
+// $arr4 = array_flip($arr4);
+// $keys = array_keys($arr4);
+
+// $values = array_values($arr4);
+
+// function isPrime($number)
+// {
+//     if($number < 2)
+//         return false;
+//     if($number == 2)
+//         return true;
+
+//     for($div = 2; $div * $div < $number; $div++)
+//     {
+//         if($number % $div == 0)
+//             return false;
+//     }
+
+//     return true;
+// }
+
+// $numbers = range(1,20);
+
+// $mapped_numbers = array_map(fn($number) => "Number: " . $number, $numbers);
+
+// $filtered_numbers = array_filter($numbers, fn($number) => isPrime($number));
+
+// $sum = array_reduce($filtered_numbers, fn($carry,$number) => $carry + $number);
+
+// print_r($sum);
+
+
+
+
 
 
 ?>
