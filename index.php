d0fdcdba4cc24d64a4869c1c823a1de2427258d9
omernawaz <omer.nawaz@arbisoft.com>
2024-06-27 12:59:55 +0500

    OOP

--- index.php
+++ index.php
@@ -1,19 +1,46 @@
 <?php
-function inverse($x) {
-    if(!$x)
-        throw new Exception("Division by zero");
 
-    return 1/$x;
+class User {
+    private $username;
+    private $email;
+    private $password;
+
+    public function __construct($_username, $_email, $_password)
+    {
+        $this->username = $_username;
+        $this->email = $_email;
+        $this->password = $_password;
+    }
+
+    public function set_name($_username)
+    {
+        $this->username = $_username;
+    }
+
+    public function get_name()
+    {
+        return $this->username;
+    }
 }
 
+class Employee extends User {
 
-try {
-    echo inverse(5);
-    echo inverse(0);
-} catch(Exception $e) {
-    echo '<br>Exception Caught: ' . $e->getMessage() . ' <br>';
-} finally {
-    echo "Continue";
+    private $title;
+
+    public function __construct($_username, $_email, $_password, $_title)
+    {
+        parent::__construct($_username,$_email,$_password);
+        $this->title = $_title;
+    }
+
+    public function get_title()
+    {
+        return $this->title;
+    }
 }
+
+$employee = new Employee("Omer", "omer@arbi.com", "1234", "trainee");
+
+var_dump($employee);
 
 ?>
