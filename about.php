71bb4e1da8b811aa8e6642c4b1f0f61843c8d4d8
omernawaz <omer.nawaz@arbisoft.com>
2024-06-27 16:17:07 +0500

    Finished about page

--- about.php
+++ about.php
@@ -1,3 +1,20 @@
 <?php include 'includes/header.php' ?>
-<?php include 'includes/navbar.php' ?>
-<?php include 'includes/footer.php' ?>
+
+
+<body>
+    <?php include 'includes/navbar.php' ?>
+
+    <div class = 'container-fluid justify-content-center text-center p-5'>
+        <h2 class= 'text-primary'>About Arbisoft</h2>
+    </div>  
+
+    <div class = 'container-fluid justify-content-center text-center w-50'>
+        <p>Arbisoft specializes in rapid application development, Enterprise Applications, 3D Applications, Mobile App Development, 
+            Automated Data Scraping, Cloud Applications, data services, SaaS, Python. Arbisoft employs 1,023 employees. The Arbisoft management team includes Yasser Bashir (Chief Executive Officer), 
+            Umair Shah (Co-Founder and Managing Partner), and Zawar Qayyum (Director Software Services). We are a full-scale software development firm that knows the value of lasting relationships and can combine focused detail-oriented action with long-term strategic thinking.
+        </p>
+    </div>  
+
+    
+    <?php include 'includes/footer.php' ?>
+</body>
