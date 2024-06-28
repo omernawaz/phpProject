dfa3c469b4f6a301e3a8dfa62b78a6e8734552b8
omernawaz <omer.nawaz@arbisoft.com>
2024-06-28 11:06:33 +0500

    Fixed nav-pills on navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,10 @@
 <?php $current_page = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1); 
 $inactive = 'nav-link';
 $active = 'nav-link active';
+
+// echo $current_page == 'index.php' ? $active : $inactive;
+// echo $current_page == 'feedback.php' ? $active : $inactive;
+// echo $current_page == 'about.php' ? $active : $inactive;
 ?>
 
 
@@ -13,13 +17,13 @@ $active = 'nav-link active';
 
     <ul class="nav nav-pills justify-content-end">
         <li class="nav-item">
-            <a class=<?php echo $current_page == 'index.php' ? $active : $inactive ?> href="index.php">Feedback Form</a>
+            <a class="<?php echo $current_page == 'index.php' ? $active : $inactive ?>" href="index.php">Feedback Form</a>
         </li>
         <li class="nav-item">
-            <a class=<?php echo $current_page == 'feedback.php' ? $active : $inactive ?> href="feedback.php">Past Feedback</a>
+            <a class="<?php echo $current_page == 'feedback.php' ? $active : $inactive ?>" href="feedback.php">Past Feedback</a>
         </li>
         <li class="nav-item">
-            <a class=<?php echo $current_page == 'about.php' ? $active : $inactive ?> href="about.php">About</a>
+            <a class="<?php echo $current_page == 'about.php' ? $active : $inactive ?>" href="about.php">About</a>
         </li>
     </ul>
   </div>
